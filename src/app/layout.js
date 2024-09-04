63c6c15e060132d0d03406d2b50c76f7aa72c601
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-09-04 09:44:11 +0530

    session Provider template complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import News from "@/components/News";
 import Sidebar from "@/components/Sidebar";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,25 +13,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex justify-between max-w-6xl mx-auto">
-          <div className="hidden sm:inline border-r h-screen">
-            <Sidebar />
-          </div>
-          <div>{children}</div>
-          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
-            <div className="sticky top-0 bg-white py-2">
-              <input
-                type="text"
-                placeholder="Search"
-                className="bg-violet-50 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
-              ></input>
+    // add sessionWrapper
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex justify-between max-w-6xl mx-auto">
+            <div className="hidden sm:inline border-r h-screen">
+              <Sidebar />
             </div>
-            <News />
+            <div>{children}</div>
+            <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+              <div className="sticky top-0 bg-white py-2">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="bg-violet-50 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+                ></input>
+              </div>
+              <News />
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
