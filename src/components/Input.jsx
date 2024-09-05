b039c1e68562f5e24667472d789919b9677fa125
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-09-05 21:55:23 +0530

    data adding to cloud firestore

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,23 +1,32 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
+'use client';
 
-import { useSession } from "next-auth/react";
-import { HiOutlinePhotograph } from "react-icons/hi";
-
-import { app } from "../firebase";
+import { useSession } from 'next-auth/react';
+import { useEffect, useRef, useState } from 'react';
+import { HiOutlinePhotograph } from 'react-icons/hi';
+import { app } from '../firebase';
 import {
   getStorage,
   ref,
   uploadBytesResumable,
   getDownloadURL,
-} from "firebase/storage";
+} from 'firebase/storage';
+
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp,
+} from 'firebase/firestore';
+
 export default function Input() {
   const { data: session } = useSession();
-  const [selectedFile, setSelectedFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null); // [1
+  const [imageFileUploading, setImageFileUploading] = useState(false); // [2
+  const [text, setText] = useState(''); // [3
+  const [postLoading, setPostLoading] = useState(false); // [4
   const imagePickRef = useRef(null);
-  const [imageFileUploading, setImageFileUploading] = useState(false);
-
+  const db = getFirestore(app);
   const addImageToPost = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -35,15 +44,15 @@ export default function Input() {
   const uploadImageToStorage = () => {
     setImageFileUploading(true);
     const storage = getStorage(app);
-    const fileName = new Date().getTime() + "-" + selectedFile.name;
+    const fileName = new Date().getTime() + '-' + selectedFile.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, selectedFile);
     uploadTask.on(
-      "state_changed",
+      'state_changed',
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
+        console.log('Upload is ' + progress + '% done');
       },
       (error) => {
         console.log(error);
@@ -60,42 +69,63 @@ export default function Input() {
     );
   };
 
+  const handleSubmit = async () => {
+    setPostLoading(true);
+    const docRef = await addDoc(collection(db, 'posts'), {
+      uid: session.user.uid,
+      name: session.user.name,
+      username: session.user.username,
+      text,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+      image: imageFileUrl,
+    });
+    setPostLoading(false);
+    setText('');
+    setImageFileUrl(null);
+    setSelectedFile(null);
+  };
+
   if (!session) return null;
   return (
-    <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
+    <div className='flex border-b border-gray-200 p-3 space-x-3 w-full'>
       <img
         src={session.user.image}
-        alt="user-img"
-        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+        alt='user-img'
+        className='h-11 w-11 rounded-full cursor-pointer hover:brightness-95'
       />
-      <div className="w-full divide-y divide-gray-200">
+      <div className='w-full divide-y divide-gray-200'>
         <textarea
-          className="w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700 "
-          placeholder="Whats happening"
-          rows="2"
+          className='w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700 '
+          placeholder='Whats happening'
+          rows='2'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></textarea>
         {selectedFile && (
           <img
             src={imageFileUrl}
-            alt="image"
-            className="max-h-[250px] object-contain cursor-pointer"
+            alt='image'
+            className={`w-full max-h-[250px] object-cover cursor-pointer
+            ${imageFileUploading ? 'animate-pulse' : ''}`}
           />
         )}
-        <div className="flex items-center justify-between pt-2.5">
+        <div className='flex items-center justify-between pt-2.5'>
           <HiOutlinePhotograph
             onClick={() => imagePickRef.current.click()}
-            className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer"
+            className='h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer'
           />
           <input
-            type="file"
+            type='file'
             ref={imagePickRef}
-            accept="image/*"
+            accept='image/*'
             onChange={addImageToPost}
             hidden
           />
           <button
-            disabled
-            className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+            disabled={text.trim() === '' || postLoading || imageFileUploading}
+            className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+            onClick={handleSubmit}
           >
             Post
           </button>
